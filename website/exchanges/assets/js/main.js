732efaf0fe76b920a7a2f9a2da861bca1c065f34
thyocp <143692394+thyocp@users.noreply.github.com>
2023-12-21 13:48:11 +0800

    add websites

---
+++ website/exchanges/assets/js/main.js
@@ -0,0 +1,37 @@
+(function ($) {
+  "use strict";
+
+  // countdown-jquery
+  $("#countdown").countdown(
+    {
+      date: "20 may 2024 09:00:00" /*Change your time here*/,
+      format: "on",
+    },
+    function () {
+      // callback function
+    }
+  );
+
+  // background-start
+  $("[data-background]").each(function () {
+    $(this).css(
+      "background-image",
+      "url(" + $(this).attr("data-background") + ")"
+    );
+  });
+
+  // preloder-heare
+  var loader = document.getElementById("preloader");
+  window.addEventListener("load", function () {
+    loader.style.display = "none";
+  });
+
+  // contact-form-sidebar
+  $(".contact-form-ep").on("click", function () {
+    $(".contact-form-open").addClass("active");
+  });
+
+  $(".contact-form-close").on("click", function () {
+    $(".contact-form-open").removeClass("active");
+  });
+})(jQuery);
